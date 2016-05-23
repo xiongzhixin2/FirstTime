a48ac2c4db8af913438bf224d15f03745d234dd0
xiongxin <xiongzhixin2@163.com>
2016-05-23 16:01:24 +0800

    nothing to commit

---
+++ app/src/main/java/com/example/kuma/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.kuma.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        Log.d("git", "init: helloworld");
+    }
+}
